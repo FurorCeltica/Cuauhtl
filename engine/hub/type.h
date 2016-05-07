dff37298a0718736986d7b103d17d8f752eb8f64
pxaramillo <pxaramillo@yahoo.com>
2016-05-07 17:34:31 +0200

    Update type.h

--- engine/hub/type.h
+++ engine/hub/type.h
@@ -1,3 +1,27 @@
+typedef unsigned double udouble;
+typedef unsigned float ufloat;
+typedef unsigned long long ulint2;
+typedef unsigned long ulint;
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef unsigned char ubyte;
+
+typedef double[2] dvec2;
+typedef double[3] dvec3;
+typedef double[4] dvec4;
+
 typedef float[2] vec2;
 typedef float[3] vec3;
 typedef float[4] vec4;
+
+typedef int[2] ivec2;
+typedef int[3] ivec3;
+typedef int[4] ivec4;
+
+typedef vec2[2] mat2x2;
+typedef vec3[2] mat3x2;
+typedef vec3[3] mat3x3;
+typedef vec4[2] mat4x2;
+typedef vec4[3] mat4x3;
+tyepdef vec4[4] mat4x4;
+
